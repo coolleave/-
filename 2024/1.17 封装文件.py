dd930228e68e12bda988d0fafc50d212743436cb
coolleave <107819165+coolleave@users.noreply.github.com>
2024-01-17 19:03:46 +0800

    Add files via upload

--- 2024/1.17 封装文件.py
+++ 2024/1.17 封装文件.py
@@ -18,11 +18,12 @@ lst_ip = []  # 准备代理列表
 def get_ip2():
     url = 'https://zhuanlan.zhihu.com/p/395461277'
     resp = requests.get(url)
-    tree = etree.HTML(resp.text)
+    tree = etree.HTML(resp.text)  # xpath 封装
+    # xpath定位所有的p标签
     ps = tree.xpath('//*[@id="root"]/div/main/div/article/div[1]/div/div/div/p')
     for p in ps:
         try:
-            text = p.xpath('./text()')
+            text = p.xpath('./text()')  # 提取下载链接
             text = text[0]
             ip = text.split("[")[0]
             http = {'https//:': f'{ip}'}
@@ -39,7 +40,6 @@ def check_proxy(proxy):
         if resp.status_code == 200:
             # print(f'{proxy}可用')
             lst_ip.append(proxy)  # ip可用，将此ip添加到ip池当中
-
             return True
         else:
             return False
@@ -73,7 +73,7 @@ def download(url):
     name = html.xpath('/html/body/div[5]/div/div[1]/div/div[1]/text()')  # 通过xpath找到小说名字
 
     # 保存文件
-    with open(f'{name[0]}.zip', 'wb') as w:
+    with open(f'./rar/{name[0]}.rar', 'wb') as w:
         w.write(requests.get('http://www.12z.cn' + download_url[0], headers=headers).content)  # 下载文件
         print(f'成功下载文件{name[0]}')
 
@@ -83,7 +83,6 @@ def unzips(folder_path):
     for file_name in os.listdir(folder_path):  # 遍历文件夹内所有的文件
         if file_name.endswith('.rar'):  # 找到rar文件
             file_path = os.path.join(folder_path, file_name)
-            extract_path = os.path.splitext(file_path)[0]
             # 解压找到的文件
             with rarfile.RarFile(file_path, 'r') as rf:
                 rf.extractall('./txt')  # 解压到txt文件夹中
@@ -92,11 +91,11 @@ def unzips(folder_path):
 def novelchapters2(book_name):
     # 创建 Workbook 对象
     wb = xlwt.Workbook()
-
     # 创建工作表
     ws = wb.add_sheet('Sheet1')
+    # print(f'{book_name}.txt')
     # 打开小说文件
-    with open(f'{book_name}.txt', 'r', encoding='gb18030') as r:
+    with open(f'./txt/{book_name}.txt', 'r', encoding='gb18030') as r:
         txt = r.read()
         lst = txt.split('\n\n')  # 用双换行符切
         # 写入表格
@@ -109,7 +108,7 @@ def novelchapters2(book_name):
             lst[i] = re.sub('\n', r'\\n', lst[i], flags=re.DOTALL)  # 把换行符替换成\n
             # print(lst[1])
             # worksheet.cell(row=i, column=1, value=lst[i])
-            if lst[i] == '\\n' or lst[i].startswith('内容简介'):
+            if lst[i] == '\\n' or lst[i].startswith('内容简介') or lst[i].startswith('\\n内容简介'):
                 a += 1
             else:
                 ws.write(i-1-a, 0, lst[i])
@@ -120,12 +119,18 @@ def novelchapters2(book_name):
         print(f'{book_name} over!')
 
 
-def handle_txt():
-    
+# 找到txt文件名称，并传入章节处理函数
+def handle_txt(folder_path):
+    for file_name in os.listdir(folder_path):  # 遍历文件夹内所有的文件
+        if file_name.endswith('.txt'):  # 找到txt文件名称
+            file_name = file_name.split('.')[0]  # 切走后缀
+            print(file_name)
+            novelchapters2(file_name)
+
 
 if __name__ == '__main__':
-    # unzips('./rar')
-    # get_urls()
-    handle_txt()
-
+    get_ip2()  # 拿到代理ip池
+    get_urls()  # 拿到并下载rar文件
+    unzips('./rar')  # 解压文件
+    handle_txt('./txt')  # 处理小说章节， 并写入excel
 
