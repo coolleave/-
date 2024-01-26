f19842e307c5e27dc53cbfb473d359cc314f54e1
coolleave <107819165+coolleave@users.noreply.github.com>
2024-01-26 08:26:54 +0800

    Add files via upload

--- 2024/1.25 面向对象.py
+++ 2024/1.25 面向对象.py
@@ -19,18 +19,51 @@ def test1():
 
 
 def test2():
+    # 创建Person类
     class Person:
+        # 初始化 输入名字 年龄 性别
         def __init__(self, name, age, sex):
             self.name = name
             self.age = age
             self.sex = sex
 
-    class relationship:
-        relationship_lst = []
+    # 创建Relationship 类
+    class Relationship:
+        # 初始化 准备列表存储关系
+        def __init__(self):
+            self.relationship_lst = []
+        # 结婚方法，传入两个变量
 
-        def relationship(self, p1, p2):
+        def make_couple(self, obj1, obj2):
+            self.relationship_lst = [obj1, obj2]
 
+        # 查询婚姻关系方法
+        def check_couple(self, obj):
+            # 通过循环找到另一半的实例，返回另一半的实例
+            for i in self.relationship_lst:
+                if i != obj:
+                    # print(i.name)
+                    return i
+            print(None)
+
+        # 离婚方法，清空列表
+        def divorce(self):
+            self.relationship_lst = []
+
+    # 将两个人实例化
+    p1 = Person('小明', '22', 'm')
+    p2 = Person('小红', '20', 'w')
+    # 将关系实例化
+    rel = Relationship()
+    # 两个人结婚
+    rel.make_couple(p1, p2)
+    # 查询婚姻状态
+    print(rel.check_couple(p1).name)
+    # 离婚
+    rel.divorce()
+    # 查询状态
+    print(rel.check_couple(p1))
 
 
 if __name__ == '__main__':
-    test1()
+    test2()
