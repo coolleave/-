5c2a1f18e14077123112d8e0fb67c7225293910a
coolleave <107819165+coolleave@users.noreply.github.com>
2024-01-29 21:43:36 +0800

    Add files via upload

--- 2024/1.29 面向对象.py
+++ 2024/1.29 面向对象.py
@@ -33,5 +33,58 @@ def test2():
     f.resp  # 调用方法时，可以不加括号，此时的方法和属性有些类似
 
 
+# 反射（也叫映射、自省）
+"""
+反射可以让程序在运行时检查并修改自身的结构，
+使得程序能够根据需要动态地创建对象、调用方法、获取和修改属性等。
+通过反射，程序可以在运行时获取到类的信息、方法的签名、注解等，并根据这些信息进行相应的操作。
+反射一共有四种方法
+hasattr   判断
+getattr   获取
+# 获取对象的属性值
+value = getattr(obj, 'attr')
+print(value)  # 输出：10
+
+# 获取对象的方法，并调用
+method = getattr(obj, 'my_method')
+method()  # 输出：This is my method
+
+# 获取对象不存在的属性值，默认返回 None
+value = getattr(obj, 'nonexistent_attr')
+print(value)  # 输出：None
+
+# 获取对象不存在的方法，默认返回 None
+method = getattr(obj, 'nonexistent_method')
+print(method)  # 输出：None
+
+# 获取对象不存在的属性值，自定义默认值
+value = getattr(obj, 'nonexistent_attr', 0)
+print(value)  # 输出：0
+setattr   赋值
+delattr   删除
+"""
+
+
+def test3():
+    class Person:
+        def __init__(self, name, age):
+            self.name = name
+            self.age = age
+
+        @staticmethod
+        def method():
+            print(124)
+            return 1
+
+        def reflection(self):
+            # if hasattr(self, 'name1'):  # 判断实例中是否有name1属性
+            #     print('yes')
+
+            a = getattr(p, 'name')
+            print(a())
+    p = Person('coollave', '20')
+    p.reflection()
+
+
 if __name__ == '__main__':
-    test1()
+    test3()
