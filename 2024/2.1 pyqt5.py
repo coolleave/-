e72a08af4c3da5147067cc9ae50b55dc84dc9f79
coolleave <107819165+coolleave@users.noreply.github.com>
2024-02-01 21:27:34 +0800

    Add files via upload

---
+++ 2024/2.1 pyqt5.py
@@ -0,0 +1,18 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget
+
+print(sys.argv)
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+
+    # 设置窗口标题
+    w.setWindowTitle("第一个PyQt")
+
+    # 展示窗口
+    w.show()
+
+    # 程序进行循环等待状态
+    app.exec()
