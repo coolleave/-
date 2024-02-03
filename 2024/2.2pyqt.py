0760208a108939263bb020ffc4835f1385591320
coolleave <107819165+coolleave@users.noreply.github.com>
2024-02-03 10:50:30 +0800

    Add files via upload

--- 2024/2.2pyqt.py
+++ 2024/2.2pyqt.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QLabel, QLineEdit
 import sys
 
 
@@ -19,5 +19,55 @@ def button():
     app.exec()
 
 
+def qlabel():
+    app = QApplication(sys.argv)
+    w = QWidget()
+    w.setWindowTitle('文本框显示')
+
+    # 创建一个纯文本对象
+    label = QLabel('请输入账号', w)
+    # 设置大小和位置
+    # (0,0) 是位置左上角是原点， 1000，1000 是宽和高
+    # 绝对定位
+    label.setGeometry(50, 50, 100, 100)
+    w.show()
+    app.exec()
+
+
+# 输入框
+def lineedit():
+    app = QApplication(sys.argv)
+    w = QWidget()
+    # 创建文本框
+    line = QLineEdit(w)
+    # 设置大小和位置
+    line.setGeometry(30, 30, 100, 30)
+    w.show()
+    app.exec()
+
+
+def test1():
+    app = QApplication(sys.argv)
+    w = QWidget()
+    w.setWindowTitle('user login')
+    w.setGeometry(300, 300, 600, 400)
+    button_login = QPushButton('login', w)
+    label_un = QLabel('uersname', w)
+    label_pw = QLabel('password', w)
+    line_un = QLineEdit(w)
+    line_pw = QLineEdit(w)
+    label_un.setGeometry(0, 40, 100, 20)
+    label_pw.setGeometry(0, 0, 100, 20)
+    line_un.setGeometry(100, 0, 100, 20)
+    line_pw.setGeometry(100, 40, 100, 20)
+    button_login.setGeometry(100, 100, 100, 30)
+    w.show()
+    app.exec()
+
+
+
 if __name__ == '__main__':
-    button()
+    # button()
+    # qlabel()
+    # lineedit()
+    test1()
