d3a8bc5844b4bbda6f6f170a58628c86ae68ba36
coolleave <107819165+coolleave@users.noreply.github.com>
2024-02-06 14:41:13 +0800

    Add files via upload

--- 2024/2.5信号与槽.py
+++ 2024/2.5信号与槽.py
@@ -8,8 +8,9 @@
 import sys
 import time
 
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication,  QWidget, QVBoxLayout, QPushButton, QScrollArea
 from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
 
 
 def test1():
@@ -69,5 +70,81 @@ def test2():
     app.exec()
 
 
+# 发送网络模拟测试（应用滚动条）
+def test3():
+    class Window(QWidget):
+        signal = pyqtSignal(str)
+
+        def __init__(self):
+
+            super().__init__()
+            self.init_ui()
+            self.msg_list = list()  # 用来存放消息
+
+        def init_ui(self):
+            self.resize(500, 200)
+            # 创建垂直布局容器
+            container = QVBoxLayout()
+
+            # 创建标签
+            self.msg = QLabel('')
+            self.msg.setWordWrap(True)
+            self.msg.resize(300, 15)
+            self.msg.setAlignment(Qt.AlignTop)  # 靠上
+            # self.msg.setStyleSheet("backgroself.msg = QLabel('')und-color: yellow; color: black;")
+
+            # 创建滚动条
+            scroll = QScrollArea()
+            scroll.setWidget(self.msg)
+
+            # 创建垂直布局器，用来添加自动滚动条
+            v_layout = QVBoxLayout()
+            v_layout.addWidget(scroll)
+
+            # 创建第二部分按钮
+            btn = QPushButton('开始检测')
+            # 创建水平布局
+            h_layout = QHBoxLayout()
+
+            h_layout.addStretch(1)  # 设置弹簧，让按钮水平居中
+            h_layout.addWidget(btn)
+            h_layout.addStretch(1)
+
+            btn.clicked.connect(self.click)
+
+            # 添加两个布局器到容器里
+            container.addLayout(v_layout)
+            container.addLayout(h_layout)
+            self.setLayout(container)
+
+            # 绑定信号和槽
+            self.signal.connect(self.solt)
+
+        def click(self):
+            # 通过enumerte(), 拿到索引和值
+            for i, ip in enumerate([f'196.128.1.{x}'] for x in range(1, 100)):
+
+                if i % 5 == 3:
+                    msg = "模拟，正在检查 %s 上的漏洞...." % ip[0]
+                    # print(msg)
+
+                    self.signal.emit(msg + '发现漏洞')
+                    time.sleep(0.1)
+
+        def solt(self, msg):
+            print(msg)
+            self.msg_list.append(msg)
+            # print(self.msg_list)
+            # 用换行符<br>拼接列表
+            self.msg.setText("<br>".join(self.msg_list))
+            self.msg.resize(440, self.msg.frameSize().height() + 15)
+            self.msg.repaint()  # 更新内容，如果不更新可能没有显示新内容
+
+    app = QApplication(sys.argv)
+    w = Window()
+    w.show()
+    app.exec()
+
+
 if __name__ == '__main__':
-    test2()
+    test3()
