b1f392233298063a43a3300cd7c3fa460c782800
coolleave <107819165+coolleave@users.noreply.github.com>
2024-03-02 16:51:49 +0800

    Add files via upload

--- project/3.2ui设计/1.py
+++ project/3.2ui设计/1.py
@@ -1,6 +1,8 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QFileDialog
+from PyQt5.QtWidgets import QLabel, QLineEdit, QTextEdit, QPushButton
+from PyQt5.QtGui import QFont
 
 
 class Window(QWidget):
@@ -9,8 +11,63 @@ class Window(QWidget):
         self.my_ui()
 
     def my_ui(self):
+        # 设置标题和大小
         self.setWindowTitle("mainwindow")
-        self.frameSize()
+        self.resize(670, 460)
+        # 创建一个容器
+        container = QVBoxLayout()
+
+        # 创建标题
+        hbox_title = QHBoxLayout()
+        title = QLabel('多层次情感分类系统', self)
+        font_title = QFont('Arial', 30)  # 创建一个字体对象，设置字体为 Arial，大小为 16
+        title.setFont(font_title)
+        title.adjustSize()  # 调整 Label 的大小以适应文本内容
+        hbox_title.addStretch(1)
+        hbox_title.addWidget(title)
+        hbox_title.addStretch(1)
+
+        # 创建提示文本
+        hbox_tip = QHBoxLayout()
+        tip = QLabel('选择你要运行的数据集：', self)
+        tip.move(10, 100)
+        font_tip = QFont('Arial', 18)
+        tip.setFont(font_tip)
+        hbox_tip.addWidget(tip)
+        hbox_tip.addStretch(1)
+
+        # 创建文本显示框
+        hbox_load = QHBoxLayout()
+        button_load = QPushButton('...', self)
+        button_load.clicked.connect(self.upload_file)
+        text_input = QLineEdit()
+        hbox_load.addWidget(text_input)
+        hbox_load.addWidget(button_load)
+
+
+        # 创建按钮区域
+        hbox_button = QHBoxLayout()
+        button1 = QPushButton('模型', self)
+        button2 = QPushButton('运行', self)
+        hbox_button.addWidget(button1)
+        hbox_button.addWidget(button2)
+
+        # 创建文本显示区域
+        text_ouput = QTextEdit()
+
+        container.addLayout(hbox_title)
+        container.addLayout(hbox_tip)
+        container.addLayout(hbox_load)
+        container.addLayout(hbox_button)
+        container.addWidget(text_ouput)
+        self.setLayout(container)
+
+    def upload_file(self):
+        file_dialog = QFileDialog()
+        file_path, _ = file_dialog.getOpenFileName(self, '选择文件', '', 'All Files (*);;Text Files (*.txt)')  # 设置文件过滤器
+
+        if file_path:
+            print('已选择文件:', file_path)
 
 
 if __name__ == '__main__':
