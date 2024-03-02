116d17bcccadfa4c0118b89c0c4125c30844f4e3
coolleave <107819165+coolleave@users.noreply.github.com>
2024-03-02 15:53:48 +0800

    Add files via upload

--- project/3.2ui设计/1.py
+++ project/3.2ui设计/1.py
@@ -1,1 +1,21 @@
+import sys
 
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.my_ui()
+
+    def my_ui(self):
+        self.setWindowTitle("mainwindow")
+        self.frameSize()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = Window()
+
+    w.show()
+    app.exec()
