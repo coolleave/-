e229461a013d10e95ab6323167620b334ef11c0a
coolleave <107819165+coolleave@users.noreply.github.com>
2024-03-07 14:31:33 +0800

    Add files via upload

--- 2024/3.6gradio.py
+++ 2024/3.6gradio.py
@@ -17,5 +17,34 @@ def test1():
     iface.launch()
 
 
+# 总结一下gradio的输入和输出
+def test2():
+
+    input_list = [
+        # 语音
+        gr.Audio(sources=['microphone', 'upload'], label='audio'),
+        # 单选框
+        gr.Checkbox(['option1', 'option2', 'option3'], label='checkbox'),
+        # 复选框
+        gr.CheckboxGroup(['option1', 'option2', 'option3']),
+        # 颜色选择器
+        gr.ColorPicker(),
+        # 表格选择器
+        gr.DataFrame(headers=3),
+
+        gr.Dropdown(['option1', 'option2', 'option3']),
+
+        gr.File(),
+
+        gr.Image(),
+
+        gr.Number(),
+
+        gr.Radio(),
+
+
+    ]
+
+
 if __name__ == '__main__':
-    test1()
+    test2()
