98b0a1da398db06615bf82ddd23304f8a192f541
coolleave <107819165+coolleave@users.noreply.github.com>
2024-03-09 18:47:52 +0800

    Add files via upload

---
+++ 3.8gradio/ui.py
@@ -0,0 +1,19 @@
+import gradio as gr
+import random
+
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox()
+    with gr.Row():
+        clear = gr.ClearButton([msg, chatbot])
+        submit_btn = gr.Button('提交', variant='primary')
+
+    def respond(message, chat_history):
+        bot_message = random.choice(["How are you?", "I love you", "I'm very hungry"])
+        chat_history.append((message, bot_message))
+        return "", chat_history
+
+    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+
+demo.launch()
