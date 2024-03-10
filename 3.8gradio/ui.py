61290d09b6ca46899ac300abcd4ccfb9f21d7e5a
coolleave <107819165+coolleave@users.noreply.github.com>
2024-03-10 22:21:25 +0800

    Add files via upload

--- 3.8gradio/ui.py
+++ 3.8gradio/ui.py
@@ -13,7 +13,6 @@ with gr.Blocks() as demo:
         bot_message = random.choice(["How are you?", "I love you", "I'm very hungry"])
         chat_history.append((message, bot_message))
         return "", chat_history
-
+    submit_btn.click(respond,[msg, chatbot], [msg, chatbot])
     msg.submit(respond, [msg, chatbot], [msg, chatbot])
-
 demo.launch()
