54ab12800f3e512fedcae898764bbab1f174fd3f
coolleave <107819165+coolleave@users.noreply.github.com>
2024-03-22 22:58:21 +0800

    Add files via upload

--- pytorch/3.22.py
+++ pytorch/3.22.py
@@ -1,1 +1,14 @@
 
+# loss = (wx+b - y) ** 2 使得loss逐渐减少到0的点
+
+# 传入参数，b，w 样本点集
+def get_error(b, w, points):
+    total_loss = 0
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        # 累加总loss值
+        total_loss += (w*x+b - y) ** 2
+
+        # 返回平均loss值
+    return total_loss/float(len(points))
