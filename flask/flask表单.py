db2e1934b6bd80814d14ede6ff0e3746f004bbba
coolleave <107819165+coolleave@users.noreply.github.com>
2024-07-10 14:58:13 +0800

    Add files via upload

--- flask/flask表单.py
+++ flask/flask表单.py
@@ -1,1 +1,27 @@
+from flask import Flask, render_template, request, abort
 
+
+app = Flask(__name__)
+
+
+@app.route("/index", methods=["get", "post"])
+def info():
+    if request.method == "GET":
+        return render_template("index.html")
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        print(username, password)
+        if username == "admin" and password == "123":
+            return "login successedly"
+    else:
+        abort(404)
+
+
+@app.errorhandler(404)
+def handle_404_error(err):
+    return render_template("hand_404.html")
+
+
+if __name__ == '__main__':
+    app.run()
